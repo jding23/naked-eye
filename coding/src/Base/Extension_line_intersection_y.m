4a9cf57e914eb36ecfae58945dd71a2d76160bfc
jding23 <167780083+jding23@users.noreply.github.com>
2025-04-29 14:13:36 +0800

    Delete coding directory

--- coding/src/Base/Extension_line_intersection_y.m
+++
@@ -1,7 +0,0 @@
-function point = Extension_line_intersection_y(x,y,z,x1,y1,z1,screen)
-% 
-k = (screen - y1)/(y1-y);
-x0 = (k+1) * x1 - k * x;
-z0 = (k+1) * z1 - k * z;
-point = [x0,z0];
-end
--- coding/src/Base/which_pixels_x.m
+++
@@ -1,5 +0,0 @@
-function which_pixel = which_pixels_x(y,z,py,pz,pixel)
-num_y = ceil((py - y(1,1))/pixel);
-num_z = ceil((pz - z(1,1))/pixel);
-which_pixel = [num_z,num_y];
-end
